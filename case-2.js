048cf34590e78c35714fd3c21e8dacf2e842ed29
RafaniSalsabilah <120432495+RafaniSalsabilah@users.noreply.github.com>
2023-09-08 11:22:05 +0700

    Update case-2.js

--- case-2.js
+++ case-2.js
@@ -1,6 +1,6 @@
 checkTypeNumber = (givenNumber) => {
     if (typeof givenNumber == 'undefined') { //tipe data undefined
-        return "Error : Bro where is the parameter?"
+        return "Error : Bro where is the parameter?" 
     } else {
         if (typeof givenNumber == 'number') { //tipe data selain undefined
             if (givenNumber % 2 == 0) { //angka habis dibagi 2
@@ -16,7 +16,7 @@ checkTypeNumber = (givenNumber) => {
 
 console.log(checkTypeNumber(10))
 console.log(checkTypeNumber(3))
-console.log(checkTypeNumber("3"))
-console.log(checkTypeNumber({}))
-console.log(checkTypeNumber([]))
-console.log(checkTypeNumber())
+console.log(checkTypeNumber("3")) //tipe data tidak sesuai karena yang diminta number
+console.log(checkTypeNumber({})) //tipe data tidak sesuai karena yang diminta number
+console.log(checkTypeNumber([])) //tipe data tidak sesuai karena yang diminta number
+console.log(checkTypeNumber()) // tidak ada parameter tipe data yang diminta
