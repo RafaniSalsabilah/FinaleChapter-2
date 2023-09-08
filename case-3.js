f4804c0388458bcc2c1577c7a31cf469c0d80b51
RafaniSalsabilah <120432495+RafaniSalsabilah@users.noreply.github.com>
2023-09-08 23:12:47 +0700

    Update case-3.js

--- case-3.js
+++ case-3.js
@@ -1,29 +1,27 @@
 getAngkaTerbesarKedua = (personName) => {
-    if (!Array.isArray(personName)) {
-        return "ERROR : Invalid data type and Can't read the length" //pengecekan tipe data undefined
-    } 
-
-    if (personName.length < 2) {
-      return "ERROR : Elemen dalam Index tidak tersedia" //pengecekan elemen index jika tidak sesuai dengan array yang tersedia
-    }
+    if (!Array.isArray(personName) || personName.length < 2 && typeof dataNumbers != 'object') {
+        return "ERROR : Invalid data type" //pengecekan apabila tipe data array != object
+    } else if (typeof getAngkaTerbesarKedua != 'undefined'){  //pengecekan apabila tipe data function != undefined
+      let besar = -Infinity //deklarasi tidak ada nilai terkecil
+      let besarBesar = -Infinity //deklarasi lanjutan atas tidak ada nilai terkecil
   
-    let besar = -Infinity //deklarasi tidak ada nilai terkecil
-    let besarBesar = -Infinity //deklarasi lanjutan atas tidak ada nilai terkecil
+      personName.map((angka) => { 
+        if (angka > besar) {
+          besar = angka;
+        } else if (angka !== besar && angka > besarBesar) {
+          besarBesar = angka;
+        }
+      })
+      return besarBesar
 
-    personName.map((angka) => {
-      if (angka > besar) {
-        besar = angka;
-      } else if (angka !== besar && angka > besarBesar) {
-        besarBesar = angka;
-      }
-    })
+    } else{ //output ketika data beserta parameternya tidak sesuai 
+      return "ERROR : Where is the parameter?"
+    }
 
-  return besarBesar
 }
 
-let dataNumbers = [9, 4, 7, 7, 4, 3, 2, 2, 8]//array angka yang akan dicari angka terbesar kedua
+const dataNumbers = [9, 4, 7, 7, 4, 3, 2, 2, 8]//array angka yang akan dicari angka terbesar kedua
 
 console.log(getAngkaTerbesarKedua(dataNumbers))
 console.log(getAngkaTerbesarKedua(0))
 console.log(getAngkaTerbesarKedua())
-// console.log(getAngkaTerbesarKedua([])) //-- jika ingin keluar output "ERROR : Elemen dalam Index tidak tersedia"
